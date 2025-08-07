b8a0851f7f671bcd6b38767fe38882fc9168b180
fabianodiogo8545@gmail.com <fabianodiogo8545@gmail.com>
2025-08-07 12:08:57 -0300

    atividade

--- cypress/e2e/exercicio-api.cy.js
+++ cypress/e2e/exercicio-api.cy.js
@@ -1,22 +1,54 @@
 /// <reference types="cypress" />
 
 describe('Testes da Funcionalidade Usuários', () => {
+  
 
   it('Deve validar contrato de usuários', () => {
-    //TODO: 
+
   });
+})
 
   it('Deve listar usuários cadastrados', () => {
-    //TODO: 
-  });
-
+       cy.wait(1000)
+          cy.request({
+            method: 'GET',
+             url: '/usuarios'
+             }).should((Response)=> {
+                expect(Response.status).eq(200)
+                expect(Response.body).to.have.property('usuarios')
+             })
+    })    
   it('Deve cadastrar um usuário com sucesso', () => {
-    //TODO: 
+          cy.request({
+            method: 'POST',
+              url: '/usuarios',
+               body: {
+                nome: "Fulano da Silva",
+                 email: "bltraoqa@qa.com.br",
+                   password: "teste",
+                    administrador: "true"
+    }
+              }).then((response) => {
+                 expect(response.status).to.eq(201)
+                 expect(response.body).to.have.property('message')
   });
-
-  it('Deve validar um usuário com email inválido', () => {
-    //TODO: 
+});
+     it('Deve validar um usuário com email inválido', () => {
+  cy.request({
+            method: 'POST',
+             url: '/usuarios',
+              failOnStatusCode: false,
+               body: {
+                 nome: "Fulano da Silva",
+                  email: "emailinvalido.com", 
+                   password: "123456",
+                    administrador: "true"
+    }
+             }).then((response) => {
+                expect(response.status).to.eq(400);
+                expect(response.body.email).eq('email deve ser um email válido')
   });
+});
 
   it('Deve editar um usuário previamente cadastrado', () => {
     //TODO: 
@@ -26,5 +58,5 @@ describe('Testes da Funcionalidade Usuários', () => {
     //TODO: 
   });
 
-
-});
+  
+  
